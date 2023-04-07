13d3d9cdd5759b028b692124fceab469f5a94777
SAAD <SAADRAZI1998@GMAIL.com>
2023-04-06 19:42:54 -0500

    migrate

--- db/migrate/20230407002605_create_products.rb
+++ db/migrate/20230407002605_create_products.rb
@@ -2,15 +2,11 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :string,
       t.string :price
-      t.string :integer,
       t.string :image_url
-      t.string :string,
-      t.string :description
-      t.string :string
+      t.string :description 
 
       t.timestamps
     end
-  end
-end
+  end 
+end 
